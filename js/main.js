3a53ce1f1cc15cb3b4326cd0a7111e69ecb50389
Max <max@MacBook-Pro-Max.local>
2024-05-27 21:34:26 +0300

    fixed some bugs

--- js/main.js
+++ js/main.js
@@ -1,36 +1,52 @@
 const heroNavSpan = document.querySelectorAll('.hero_nav_span');
 const heroNav = document.querySelectorAll('.hero_nav_scroll_link');
-const anchors = document.querySelectorAll('a[href*="#"]')
+const anchors = document.querySelectorAll('a[href*="#"]');
 
-const userHeight = window.innerHeight;
+let options = {
+  threshold: [0.35]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.hide_animation');
 
 window.addEventListener('scroll', e => {
   document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
 })
 
+function navBarChecker() {
 
-heroNav.forEach((event) => {
-  event.addEventListener('click', () => {
-    heroNavSpan.forEach((span) => {
-      span.classList.remove('hero_nav_span_active');
+  heroNav.forEach((event) => {
+    event.addEventListener('click', function () {
+      heroNavSpan.forEach((span) => {
+        span.classList.remove('hero_nav_span_active');
+      });
+      event.nextElementSibling.classList.add('hero_nav_span_active');
     });
-    event.nextElementSibling.classList.add('hero_nav_span_active');
   });
-});
 
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
 
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
+      const blockID = anchor.getAttribute('href').substring(1)
 
-    const blockID = anchor.getAttribute('href').substring(1)
-
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
     })
+  }
+
+  window.addEventListener('scroll', e => {
+    console.log(document.documentElement.scrollTop)
+    if (document.documentElement.scrollTop === 0) {
+      heroNavSpan.forEach((span) => {
+        span.classList.remove('hero_nav_span_active');
+      });
+      heroNavSpan[0].classList.add('hero_nav_span_active');
+    }
   })
 }
+
 
 function onEntry(entry) {
   entry.forEach(change => {
@@ -43,12 +59,8 @@ function onEntry(entry) {
   });
 }
 
-let options = {
-  threshold: [0.5]
-};
-let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.hide_animation');
-
 for (let elm of elements) {
   observer.observe(elm);
 }
+
+navBarChecker();
